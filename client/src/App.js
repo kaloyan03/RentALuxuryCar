8a98e17ee669f57d65c2c6f1e8c620b91a5eab9d
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-04-29 23:59:24 +0300

    simple routing implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,49 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Header from "./components/Header";
+
 import Login from "./components/Login";
 import Register from "./components/Register";
 
 function App() {
   return (
     <div className="site-wrapper">
-      <Login />
-      <Register />
+
+<Routes>
+          <Route path="/login" element={<Login />}>
+          </Route>
+          <Route path="/register" element={<Register />}>
+          </Route>
+        </Routes>
+      <Header></Header>
+    
+      
     </div>
   );
 }
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
 
 export default App;
