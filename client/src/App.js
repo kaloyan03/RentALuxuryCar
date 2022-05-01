469f2386b745f2ade774cb5cffe37c7678673f9f
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-05-01 22:45:12 +0300

    header placed over router

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,20 @@ import Header from "./components/Header";
 
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Cars from "./components/Cars";
 
 function App() {
   return (
     <div className="site-wrapper">
-
+<Header></Header>
 <Routes>
+          <Route path="/cars" element={<Cars />}>
+          </Route>
           <Route path="/login" element={<Login />}>
           </Route>
           <Route path="/register" element={<Register />}>
           </Route>
-        </Routes>
-      <Header></Header>
-    
-      
+        </Routes>      
     </div>
   );
 }
