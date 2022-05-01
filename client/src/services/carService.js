48f322772b78df5b9ebae1b58c996bd57d17258c
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-05-01 22:05:26 +0300

    api service for cars implemented(get all and get one)

---
+++ client/src/services/carService.js
@@ -0,0 +1,15 @@
+const baseUrl = 'http://localhost:8000';
+
+const getAll = () => {
+    return fetch(`${baseUrl}/cars-api/`, {
+        method: 'GET',
+    })
+    .then(response => response.json())
+}
+
+const getOne = (id) => {
+    return fetch(`${baseUrl}/cars-api/${id}`, {
+        method: 'GET',
+    })
+    .then(response => response.json())
+}
