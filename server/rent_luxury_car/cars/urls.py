e04dfe407316c1f0cba9ad162e4fe791f2279b78
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-05-01 21:56:45 +0300

    serializer created for car model; views for car model created;

--- server/rent_luxury_car/cars/urls.py
+++ server/rent_luxury_car/cars/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from rent_luxury_car.cars import views as car_views
 
 urlpatterns = [
-    # path()
+    path('', car_views.ListCreateCarsView.as_view(), name='list create cars'),
+    path('<int:pk>/', car_views.SingleCarView.as_view(), name='retrieve update destroy car'),
 ]
--- server/rent_luxury_car/cars/views.py
+++ server/rent_luxury_car/cars/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics as generic_views
 
 # Create your views here.
+from rent_luxury_car.cars.models import Car
+from rent_luxury_car.cars.serializers import CarSerializer
+
+
+class ListCreateCarsView(generic_views.ListCreateAPIView):
+    serializer_class = CarSerializer
+    queryset = Car.objects.all()
+
+
+class SingleCarView(generic_views.RetrieveUpdateDestroyAPIView):
+    serializer_class = CarSerializer
+    queryset = Car.objects.all()
