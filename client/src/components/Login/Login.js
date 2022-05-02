84cc179cdff56c3b02ffc8c579645e73a19693c1
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-05-02 22:44:26 +0300

    Login page styled

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,3 +1,4 @@
+import './Login.css';
 import { useState } from 'react';
 import authService from '../../services/authService.js';
 
@@ -29,20 +30,27 @@ function Login() {
 
     return (
         <section className='login-page'>
+            {/* <form className='login-form' onSubmit={handleFormSubmit}>
             <h2 className="login-page-title">
                 Login
             </h2>
-
-            <form onSubmit={handleFormSubmit}>
                 <article className='input-group'>
-                    <label htmlFor='login-email'>Email:</label>
-                    <input id='login-email' type='text' value={email} onChange={handleEmailChange} />
+                    <input id='login-email' type='text' value={email} onChange={handleEmailChange} placeholder="Email"/>
                 </article>
                 <article className='input-group'>
-                    <label htmlFor='login-password'>Password:</label>
-                    <input id='login-password' type='password' value={password} onChange={handlePasswordChange} />
+                    <input id='login-password' type='password' value={password} onChange={handlePasswordChange} placeholder="Password"/>
                 </article>
                 <input type='submit' value='Login' />
+            </form> */}
+           <form className="login-form">
+                <h1>Log in</h1>
+                            <input type="text" name="email" placeholder="E-mail" className='input-field'/>
+                            <input type="password" name="password" placeholder="Password" className='input-field'/>
+
+                <p class="login-page-register">Haven't got an account? <a href="">Register</a></p>
+                <p class="forgot_password">Forgot your password? <a href="">Reset Your Password</a></p>
+
+                <input type="submit" name="login_submit" value="Log in"/>
             </form>
         </section>
     );
