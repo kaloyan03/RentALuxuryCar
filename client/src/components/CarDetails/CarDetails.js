7017262e6757771af6e7685a2c1b2bafed13a6c3
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-05-04 13:01:59 +0300

    styled car details page

--- client/src/components/CarDetails/CarDetails.js
+++ client/src/components/CarDetails/CarDetails.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import carService from '../../services/carService';
+import './CarDetails.css';
 
 function CarDetails() {
     let { id } = useParams();
@@ -8,15 +9,42 @@ function CarDetails() {
 
     useEffect(() => {
         carService.getOne(id)
-        .then(carResult => {
-            setCar(carResult);
-        })
+            .then(carResult => {
+                setCar(carResult);
+            })
     }, [])
 
 
     return (
         <section className="car-details-page">
-            <h2>{car.brand} {car.model}</h2>
+            <h2 className='car-details-page-title'>{car.brand} {car.model}</h2>
+            <article className='car-details-page-content'>
+                <article className='car-year-production'>
+                    <h3 className='car-year-production-title'>Year of production</h3>
+                    <p className='car-year-production-content'>{car.yearCreated}</p>
+                </article>
+
+
+                <article className='car-description'>
+                    <h3 className='car-description-title'>Overview of {car.brand} {car.model}</h3>
+                    <p className='car-description-content'>{car.description}</p>
+                </article>
+                <article className='car-specifications'>
+                    <h3 className='car-specifications-title'>Technical specifications</h3>
+                    <div className='car-specifications-content'>
+                        <p>Brand: <strong>{car.brand}</strong></p>
+                        <p>Model: <strong>{car.model}</strong></p>
+                        <p>Coupe type: <strong>{car.coupeType}</strong></p>
+                        <p>Gearbox: <strong>{car.gearboxType}</strong></p>
+                        <p>Fuel consumption: <strong>{car.fuelConsumption} l/100 km</strong></p>
+                        <p>HP: <strong>{car.horsepower}</strong></p>
+                    </div>
+                </article>
+            </article>
+
+            <article className='car-image'>
+                <img src={car.image}></img>
+            </article>
         </section>
     );
 }
