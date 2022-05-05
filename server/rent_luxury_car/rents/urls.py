e288f324e7a2b457ec1796026dab2889e12e4560
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-05-05 23:02:38 +0300

    fixed names on rent urls

--- server/rent_luxury_car/rents/urls.py
+++ server/rent_luxury_car/rents/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from rent_luxury_car.rents import views as rent_views
 
 urlpatterns = [
-    path('', rent_views.ListCreateRentsView.as_view(), name='list create cars'),
-    path('<int:pk>/', rent_views.SingleRentView.as_view(), name='retrieve update destroy car'),
+    path('', rent_views.ListCreateRentsView.as_view(), name='list create rents'),
+    path('<int:pk>/', rent_views.SingleRentView.as_view(), name='retrieve update destroy rent'),
 ]
